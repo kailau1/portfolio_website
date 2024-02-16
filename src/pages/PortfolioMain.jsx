30349e1f8e79e81e3de0a09d5a2383b035569381
Kai Lau <kailau112@gmail.com>
2024-02-16 20:46:10 +0000

    Fixed portfolio page video issue.

--- src/pages/PortfolioMain.jsx
+++ src/pages/PortfolioMain.jsx
@@ -7,13 +7,7 @@ import { Container} from "@mui/material";
 const PortfolioMain = () => {
     return(
         <div className="bg">
-            <video autoPlay loop muted playsInline>
-                <source src="/images/main_background.mp4" type="video/mp4" />
-            </video>
             <NavBar />
-            <Container>
-                <video autoPlay loop muted src="/images/portfolio/main/IMG_3992.mp4" type="video/mp4" />
-            </Container>
         </div>
     )
 };
